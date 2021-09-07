1183addb40706a15e7b4f37177975b25dfbf4d88
puppy0513 <61609292+puppy0513@users.noreply.github.com>
2021-09-07 22:29:11 +0900

    css modification
    css modification

--- js/background.js
+++ js/background.js
@@ -1,9 +1,40 @@
+const body = document.querySelector("body");
+
 const images =["image1.jpg","image2.jpg","image3.jpg"];
 
-const chosenImages = images[Math.floor(Math.random() * images.length)];          // image를 random으로 선택
+const IMG_NUMBER = images.length;
 
-const bgImage = document.createElement("img");
+//image를 랜덤으로 선택하게 된다. 
+function genRandom() {
+    const number = Math.floor(Math.random() *IMG_NUMBER);
+    return number;
+}
+// const chosenImages = images[Math.floor(Math.random() * images.length)];         
 
-bgImage.src = `img/${chosenImages}`;
+function init() {
+    const randomNumber = genRandom();
+    return number;
+}
 
-document.body.appendChild(bgImage);
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `img/image${imgNumber+1}.jpg`;
+    image.classList.add('bgImage');
+    body.prepend(image);
+}
+
+
+function init(){
+    Random = genRandom();
+    paintImage(Random);
+}
+
+init();
+
+//const bgImage = document.createElement("");
+
+//bgImage.src = `img/${chosenImages}`;
+
+//console.log(bgImage.src);
+
+//document.body.appendChild(bgImage);
--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,7 @@ const todoForm = document.querySelector("#todo-form");
 const todoList = document.querySelector("#todo-list");
 const todoInput = todoForm.querySelector("input");
 
+// 할일이 들어갈 todos 배열
 let todos = [];
 
 const TODOS_KEY = "todos";
@@ -18,31 +19,31 @@ function deleteTodo(event){
 }
 
 function paintTodo(newTodo){
-    const li = document.createElement("li");
+    const li = document.createElement("li");            // li 생성
     li.id = newTodo.id;
-    const span = document.createElement("span");
-    const button = document.createElement("button");
+    const span = document.createElement("span");           //span 생성
+    const button = document.createElement("button");       // button 생성
     span.innerText= newTodo.text;
     button.innerText = "❌";
-    button.addEventListener("click",deleteTodo)
+    button.addEventListener("click",deleteTodo)             // 'X'의 클릭을 듣고 있는 Listener
     li.appendChild(span);               // li 안에 span을 생성한다.
-    li.appendChild(button); 
+    li.appendChild(button);             // li 안에 버튼을 생성한다.
     todoList.appendChild(li);
 }
 
 function handleTodoSubmit(event){
-    event.preventDefault();
-    const newTodo = todoInput.value;
+    event.preventDefault();             // submit시 기본 실행되는 event를 막는다
+    const newTodo = todoInput.value;    // newTodo는 input의 값을 받는다.
     todoInput.value = "";
 
     const newTodoObj ={
         text: newTodo,
-        id: Date.now(),
+        id: Date.now(),                 // 순서에 맞는 id 부여
     };
 
-    todos.push(newTodoObj);
-    paintTodo(newTodoObj);
-    saveTodos();
+    todos.push(newTodoObj);             // todos 배열에 push해준다. 
+    paintTodo(newTodoObj);              // 값을 화면에 출력시켜준다.
+    saveTodos();                        // 값을 localStorage에 넣어준다.
 }
 
 
@@ -50,7 +51,6 @@ function handleTodoSubmit(event){
 todoForm.addEventListener("submit",handleTodoSubmit);          // submit 이벤트를 듣고 있다. 
 
 const savedTodos = localStorage.getItem(TODOS_KEY);         // "localstorage의 todos key로 값을 가져온다. "
-
 
 if(savedTodos !== null){
     const parsedTodo  = JSON.parse(savedTodos);         // string으로 되어있는 것을 JS object로 바꿔준다. 
